a181f0b36613a4889282669a71a170a8aa08e772
Santosh Ravindra Saxena <73080416+SAXENA-SANTOSH@users.noreply.github.com>
2021-04-24 02:59:51 +0530

    Add files via upload

---
+++ Model_Training/data_preprocessing.py
@@ -0,0 +1,309 @@
+"""
+This class is used to do all sorts of required preprocessing from transformed data.
+
+developed by : Santosh Saxena
+on 19/4/2021
+
+"""
+
+#Module Declaration
+#---------------------------------------------------------------------------#
+import os
+import sys
+import kneed
+import shutil
+import pickle
+import matplotlib
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder , StandardScaler
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+#---------------------------------------------------------------------------#
+
+matplotlib.use("Agg")
+
+class Data_preprocessing:
+
+    def __init__(self,logger , path):
+        try:
+            """
+            Constructor Initialized
+
+            Input : Logger , path
+            Output : N/A
+
+            """
+            self.path = path
+            self.logger = logger
+            self.df = pd.read_csv(path + "/Input_files/transformed_data.csv")
+            self.logger.add_in_logs("Nam","Data Preprocessing")
+            self.logger.add_in_logs("BEG","Data preprocessing module initialized")
+        
+        except Exception as e:
+            self.logger.add_in_logs("ERR" , "data preprocessing in Initialization")
+            self.logger.add_in_logs("LIN" , "Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.logger.add_in_logs("TYP" , str(e))
+
+
+    def generating_directories(self):
+        
+        try:
+            """
+            This function is used for generating direcotries 
+
+            Input : N/A
+            Output : Directories will be generated
+
+            """
+            self.logger.add_in_logs("chk","Generating directories process initialized")
+            if(os.path.isdir(self.path + "/Model_files")):
+                self.logger.add_in_logs("inf","Previous model files directory detected")
+                self.logger.add_in_logs("inf","Deleting previos model files directory")
+                shutil.rmtree(self.path + "/Model_files")
+            self.logger.add_in_logs("inf","creating model files directory")
+            os.mkdir(self.path + "/Model_files")
+            self.logger.add_in_logs("inf","creating encoder directory")
+            os.mkdir(self.path + "/Model_files/Encoder")
+            self.logger.add_in_logs("inf","creating scaler directory")
+            os.mkdir(self.path + "/Model_files/Scaler")
+            self.logger.add_in_logs("inf","creating dimensionality reduction directory")
+            os.mkdir(self.path + "/Model_files/Dimensionality_reduction")
+            self.logger.add_in_logs("inf","creating cluster directory")
+            os.mkdir(self.path + "/Model_files/Cluster_directory")
+            self.logger.add_in_logs("inf","model directory is create")       
+            os.mkdir(self.path + "/Model_files/Models")
+            if(os.path.isdir(self.path + "/Input_files/Cluster_data")):
+                self.logger.add_in_logs("inf","previous cluster directory detected")
+                self.logger.add_in_logs("inf","deleting previous directory")
+                shutil.rmtree(self.path + "/Input_files/Cluster_data")
+            self.logger.add_in_logs("inf","creating cluster directory")
+            os.mkdir(self.path + "/Input_files/Cluster_data")
+            self.logger.add_in_logs("pas","generating directories process completed")
+
+        except Exception as e:
+            self.logger.add_in_logs("ERR" , "data preprocessing in generating directories")
+            self.logger.add_in_logs("LIN" , "Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.logger.add_in_logs("TYP" , str(e))
+
+
+    def scaling(self):
+
+        try:
+            """
+            The values with high scale will be standardize at a certain level in our dataset
+
+            Input : N/A
+            Output : values with high scale will be converted into a standard scaled values in out dataset
+
+
+            """
+            self.logger.add_in_logs("chk","Scaling process initialized")
+            self.logger.add_in_logs("chk","Checking missing values for the last time")
+            if(len(self.df.columns[self.df.isnull().sum() != 0]) == 0 ):
+                self.logger.add_in_logs("pas","No Missing values")
+            else:
+                for i in self.df.columns[self.df.isnull().sum() != 0]:
+                    self.logger.add_in_logs("inf", str(i) + " has missing values")
+                self.logger.add_in_logs("pas","Data having missing values")
+                raise("Data having missing values")
+            
+            sc = StandardScaler()
+            column_names = list(self.df[self.df.columns[self.df.dtypes != "object"]].columns[self.df[self.df.columns[self.df.dtypes != "object"]].max() > 10])
+            column_names.remove("SalePrice")
+            file = open(self.path + "/Model_files/scaler_column.txt" , "w")
+            temp = 0
+            for i in column_names:
+                file.write(str(i))
+                temp += 1
+                if(temp == len(column_names)):
+                    continue
+                else:
+                    file.write(" ")
+            file.close()
+            self.df[column_names] = sc.fit_transform(self.df[column_names])
+            self.logger.add_in_logs("inf","Values are scaled by standard scaler")
+            self.logger.add_in_logs("inf","saving the scaler file in scaler directory")
+            pickle.dump(sc , open(self.path+"/Model_files/Scaler/scaler.pickle","wb"))
+            self.logger.add_in_logs("pas","scaling process completed")
+            print(self.df.shape)
+        except Exception as e:
+            self.logger.add_in_logs("ERR" , "data preprocessing in scaling")
+            self.logger.add_in_logs("LIN" , "Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.logger.add_in_logs("TYP" , str(e))
+
+    def encoding(self):
+
+        try:
+            """
+            The categorical values available in the dataset will be encoded
+
+            Input : N/A
+            Output : encoded values available in our dataset
+
+            """
+
+            self.logger.add_in_logs("chk","encoding process initialized")
+            for i in self.df.columns[self.df.dtypes == "object"]:
+                encoder = LabelEncoder()
+                self.logger.add_in_logs("inf",str(i) + " is encoded")
+                self.df[i] = encoder.fit_transform(self.df[i])
+                self.logger.add_in_logs("inf",str(i) + " encoder file is saved in encoder directory")
+                pickle.dump(encoder , open(self.path + "/Model_files/Encoder/"+str(i) + ".pickle","wb"))
+            self.logger.add_in_logs("pas","encoder process completed")
+            print(self.df.shape)
+        except Exception as e:
+            self.logger.add_in_logs("ERR" , "data preprocessing in encoding")
+            self.logger.add_in_logs("LIN" , "Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.logger.add_in_logs("TYP" , str(e))
+    
+    def splitting_into_X_and_y(self):
+
+        try:
+            """
+            Splitting our dataset into X and Y
+
+            Input : N/A
+            Output : X and y 
+
+            """
+            self.logger.add_in_logs("chk","Splitting data into x and y")
+            self.X = self.df.drop(["SalePrice"] , axis = 1)
+            self.y = self.df["SalePrice"]
+
+        except Exception as e:
+            self.logger.add_in_logs("ERR" , "data preprocessing in splitting into x and y")
+            self.logger.add_in_logs("LIN" , "Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.logger.add_in_logs("TYP" , str(e))
+
+    
+    def dimensionality_reduction(self):
+        try:
+            """
+            If the dataset has high multi-collinearity it will be fixed with the dimensional reduction
+
+            Input : N/A.
+            Output : Dimensionally reduced dataset.
+
+            """
+            print(self.df.shape)
+            self.df.to_csv("/Users/santoshsaxena/Desktop/abcde.csv" , index = False)
+            self.logger.add_in_logs("chk","Dimensionality Redction process Initialized")
+            self.logger.add_in_logs("chk","find the number of dimension initialized")
+            pca = PCA()
+            temp = pca.fit_transform(self.df.drop(["SalePrice"], axis = 1))
+            no_of_features = len(self.df.columns) - 1
+            information = np.cumsum(pca.explained_variance_ratio_)
+            number_of_dimensions = (information < 0.96).sum()
+            self.logger.add_in_logs("inf",str(number_of_dimensions)+" is the number of dimensions required")
+            self.logger.add_in_logs("pas","finding number of dimension complted")
+            self.logger.add_in_logs("inf","ploting graph for dimensions vs evr")
+            plt.figure()
+            plt.style.use("classic")
+            plt.plot(range(0, no_of_features) , information ,color = "blue", label = "information vs dimensions")
+            plt.plot([number_of_dimensions, number_of_dimensions] ,[0,1] ,color = "black" ,label = "no_of_dimensions_used")
+            plt.xlabel("Number of Dimensions")
+            plt.ylabel("Information of Data")
+            plt.legend(loc="lower left")
+            plt.savefig(self.path + "/static/plots/Dimensions.jpg")
+            self.logger.add_in_logs("inf","saving plot in plots directory")
+            print("Number of dimensions is : " , number_of_dimensions)
+            pca = PCA(n_components=number_of_dimensions)
+
+            self.logger.add_in_logs("chk","Transforming data according to our dimensions")
+            self.df = pca.fit_transform(self.df.drop(["SalePrice"], axis = 1))
+            self.df = pd.DataFrame(self.df)
+            self.df["SalePrice"] = self.y
+            pickle.dump(pca, open(self.path+"/Model_files/Dimensionality_reduction/PCA.pickle","wb"))
+            self.logger.add_in_logs("pas","Dimensionality Reduction Completed")
+            self.logger.add_in_logs("pas","Demensionality reduction process completed")
+
+        except Exception as e:
+            self.logger.add_in_logs("ERR" , "data preprocessing in removing multi-collinearity")
+            self.logger.add_in_logs("LIN" , "Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.logger.add_in_logs("TYP" , str(e))
+            
+    def data_clustering(self):
+
+        try:
+            """
+            Dataset is divided into clustes so similar data get trained in a particular model
+
+            Input : N/A
+            Output : Clusters will be generated and dataset will be stored in a particular directory
+            """
+            self.logger.add_in_logs("chk","Clustering process Initialized")
+            self.logger.add_in_logs("chk","finding number of clusters")
+            self.wcss = []
+            x = self.df.drop(["SalePrice"], axis = 1)
+            self.logger.add_in_logs("inf","saving a plot of wcss vs number of clusters ")
+            for i in range(1,30):
+                model = KMeans(n_clusters= i)
+                model.fit(x)
+                self.wcss.append(model.inertia_)
+            k = kneed.KneeLocator(range(1,30),self.wcss,curve="convex", direction="decreasing")
+            self.knee = k.knee
+            self.logger.add_in_logs("inf",str(self.knee)+" number of clusters will be formed")
+            self.wcss = np.array(self.wcss)/max(self.wcss)
+            plt.figure()
+            plt.style.use("classic")
+            plt.plot(range(1,30), self.wcss , label = "WCSS vs Inertia", color = "blue")
+            plt.plot([self.knee,self.knee],[min(self.wcss),1], label = "Number of cluster used", color = "black")
+            plt.xlabel("No of cluster")
+            plt.ylabel("WCSS")
+            plt.legend(loc = "upper right")
+            plt.savefig(self.path + "/static/plots/Cluster.jpg")
+            self.logger.add_in_logs("pas","finding number of clusters completed")
+            
+            self.logger.add_in_logs("chk","getting cluster number for dataset")
+            model = KMeans(n_clusters= self.knee)
+            x = self.df.drop(["SalePrice"], axis = 1)
+            model.fit(x)
+            cluster_no = model.predict(x)
+            self.df["Cluster"] = cluster_no
+            self.logger.add_in_logs("inf","saving clustering model in model file")
+            pickle.dump(model,open(self.path+"/Model_files/Cluster_directory/Cluster.pickle","wb"))
+
+            for i in range(0,self.knee):
+                self.logger.add_in_logs("inf", str(i) + " cluster is exported in .csv file")
+                self.df[self.df["Cluster"] == i].to_csv(self.path + "/Input_files/Cluster_data/"+str(i)+"_cluster.csv", index = False)
+            self.logger.add_in_logs("pas","Exporting Clustered dataset Completed")
+
+            self.logger.add_in_logs("pas","Clustering process Completed")
+        except Exception as e:
+            self.logger.add_in_logs("ERR" , "data preprocessing in data clustering")
+            self.logger.add_in_logs("LIN" , "Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.logger.add_in_logs("TYP" , str(e))
+        
+    def data_preprocessing_package(self):
+        try:
+            """
+            Data Preprocessing pipeline
+
+            Input : N/A
+            Output : N/A
+
+            """
+            self.generating_directories()
+            self.scaling()
+            self.encoding()
+            self.splitting_into_X_and_y()
+            self.dimensionality_reduction()
+            self.data_clustering()
+            self.logger.add_in_logs("END","Data Preprocessing module completed")
+        except Exception as e:
+            self.logger.add_in_logs("ERR" , "data preprocessing in data clustering")
+            self.logger.add_in_logs("LIN" , "Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.logger.add_in_logs("TYP" , str(e))
+            
+    
+
+
+
+
+
+            
+
+
---
+++ Prediction/prediction.py
@@ -0,0 +1,263 @@
+"""
+This Module is used for prediction 
+
+Developed by Santosh Saxena 
+on 24/4/2021
+
+"""
+
+# Module declaration
+#------------------#
+import re
+import sys
+import os
+import json
+import shutil
+import pickle
+import numpy as np 
+import pandas as pd 
+#-------------------#
+
+class Prediction:
+    def __init__(self,path , file_path , runtime_logger):
+        try:
+            """
+            Prediction Module Initialization 
+
+            Input : path , file path , runtime logger
+            Output : N/A
+
+            """
+            self.path = path
+            self.file_path = file_path
+            self.runtime_logger = runtime_logger
+            self.condition = True
+            self.file_name = self.file_path.split("/")[-1]
+            self.df = pd.read_csv(file_path)
+            self.categorical_unique_values = json.load(open(self.path+"/Source_of_Truth/Data_categorical.json","r"))
+            self.no_of_date = json.load(open(path+"/Source_of_Truth/File_validation.json","r"))["no_date"]
+            self.no_of_day = json.load(open(path+"/Source_of_Truth/File_validation.json","r"))["no_day"]
+            column = open(self.path+"/Source_of_Truth/Data_columns.txt","r")
+            self.column = column.read().split(",")
+            self.cluster = pickle.load(open(self.path + "/Model_files/Cluster_directory/Cluster.pickle", "rb"))
+            self.pca = pickle.load(open(self.path + "/Model_files/Dimensionality_reduction/PCA.pickle", "rb"))
+            self.sc_x = pickle.load(open(self.path + "/Model_files/Scaler/scaler.pickle","rb"))
+            self.datatype = pd.read_csv(self.path+"/Source_of_Truth/Data_type.csv", index_col="Unnamed: 0")
+            self.datatype.drop(["SalePrice"] , inplace = True)
+            self.file_validation = json.load(open(self.path+"/Source_of_Truth/File_validation.json","r"))
+        except Exception as e:
+            self.runtime_logger.add_in_logs("Faced an error")
+            self.runtime_logger.add_in_logs("Path is not correct")
+
+
+    def file_structure_checker(self, message):
+
+        """
+        This function will check the structure of file name are valid or not.
+
+        Input : Message
+        Output : True/False , Boolean value
+
+        """
+
+        try:
+            file_name_expression = "['House']+['_']+['\d']+['_']+['\d']+[.csv]"
+            if re.match(file_name_expression , message):
+                return True
+            else:
+                return False
+        except Exception as e:
+            self.runtime_logger.add_in_logs("Faced an error in code")
+            self.runtime_logger.add_in_logs("Prediction in file structure checker")
+            self.runtime_logger.add_in_logs("Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.runtime_logger.add_in_logs(str(e))
+
+    def name_validations(self):
+        try:
+            """
+            This function will validation the given file on the basis of name. If passed then moved further validations
+            or prediction will not be done .
+
+            Input : N/A
+            Output : our variable i.e self.condition value will be decided
+
+            """
+            i = self.file_name
+            if self.file_structure_checker(i):
+                if(i[-4::1] == ".csv"):
+                    i = i.split(".")[0]
+                    i = i.split("_") 
+                    if(i[0] == "House" and len(i[1]) == self.no_of_date and len(i[2]) == self.no_of_day):
+                        self.runtime_logger.add_in_logs("File name is matched with requirements")
+                    else:
+                        self.condition = False
+                        self.runtime_logger.add_in_logs("Faced an error")
+                        self.runtime_logger.add_in_logs("File not matched with requirements")
+                else:
+                    self.condition = False
+                    self.runtime_logger.add_in_logs("Faced an error")
+                    self.runtime_logger.add_in_logs("File not matched with requirements")
+            else:
+                self.condition = False
+                self.runtime_logger.add_in_logs("Faced an error")
+                self.runtime_logger.add_in_logs("File not matched with requirements")
+        except Exception as e:
+            self.runtime_logger.add_in_logs("Faced an error in code")
+            self.runtime_logger.add_in_logs("Prediction in name validation")
+            self.runtime_logger.add_in_logs("Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.runtime_logger.add_in_logs(str(e))
+
+    def data_type_validation(self, df):
+
+        """
+        This function will check the data types of the comming data with respect to the Source_of_Truth
+        
+        Input : Dataframe
+        Ouput : fault , Integer
+
+        """
+        
+        try:
+            fault = 0
+            for i in self.df.columns:
+                if(self.df[i].dtypes == self.datatype.loc[i]["SOT"]):
+                    continue
+                else:
+                    fault += 1
+
+            return fault
+        except Exception as e:
+            self.runtime_logger.add_in_logs("Faced an error")
+            self.runtime_logger.add_in_logs( "Data validation in data type validation")
+            self.runtime_logger.add_in_logs("Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.runtime_logger.add_in_logs(str(e))
+
+
+    def unique_categorical_values(self, df):
+
+        """
+        This function will check if any random categorical values are there which are not present in the Source of Truth at 
+        that time that dataset will move to towards bad dataset
+
+        Input : Dataframe
+        Output : fault , Integer
+
+        """
+        try:
+            fault = 0
+            for i in df.columns[df.dtypes == "object"]:
+                cat_sot_values = set(self.categorical_unique_values[i])
+                cat_df_values = set(pd.Categorical(df[i]).categories)
+                if(cat_df_values.issubset(cat_sot_values)):
+                    continue
+                else:
+                    fault += 1
+            return fault
+        except Exception as e:
+            self.runtime_logger.add_in_logs("Faced an error")
+            self.runtime_logger.add_in_logs("Data validation in unique categorical values")
+            self.runtime_logger.add_in_logs("Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.runtime_logger.add_in_logs(str(e))
+
+
+
+    def data_validation(self):
+        try:
+            """
+            This function will validate the prediction file on the basis of data available in the file
+            If all the validation conditions are satisfied then only it will be predicted
+            
+            Input : N/A
+            Output : self.condition valriable value will be decided
+            
+            """
+
+            self.column.remove("SalePrice")
+            if(list(self.df.columns) != self.column):
+                self.condition = False
+                self.runtime_logger.add_in_logs("Data columns not matched the standard requirements")
+            #elif(len(self.df) != self.file_validation["no_of_rows"]):
+            #    self.condition = False
+            #    self.runtime_logger.add_in_logs("Data number of rows not matched the standard requirements")
+            elif(self.data_type_validation(self.df) != 0):
+                self.condition = False
+                self.runtime_logger.add_in_logs("Data type not matched the standard requirements")
+            elif(self.unique_categorical_values(self.df) != 0):
+                self.condition = False
+                self.runtime_logger.add_in_logs("Categories not matched the standard requirements")
+            else:
+                self.runtime_logger.add_in_logs("Data is matched with the requirements")
+            
+        except Exception as e:
+            self.runtime_logger.add_in_logs("Faced an error")
+            self.runtime_logger.add_in_logs("Prediction in Data Validation")
+            self.runtime_logger.add_in_logs("Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.runtime_logger.add_in_logs(str(e))
+
+    def prediction(self):
+        try:
+            """
+            The main prediction function . After passing through all the validation this function wil predict
+            the dataset sent by the user
+
+            Input : N/A
+            Output : predicted value dataframe
+
+            """
+            if(len(self.df.columns[self.df.isnull().sum() != 0]) > 0):
+                self.runtime_logger.add_in_logs("Data has missing values")
+            else:
+                self.runtime_logger.add_in_logs("Data has no missing values")
+                x = self.df.copy()
+                file = open(self.path + "/Model_files/scaler_column.txt","r")
+                column = str(file.read()).split(" ")
+                x[column] = self.sc_x.transform(x[column])
+                for i in os.listdir(self.path + "/Model_files/Encoder/"):
+                    encoder = pickle.load(open(self.path + "/Model_files/Encoder/" + str(i) , "rb"))
+                    name = i.split("/")[-1]
+                    name = name.split(".")[0]
+                    x[name] = encoder.transform(x[name])
+                x.drop(["Id"], axis = 1 , inplace = True)
+                result = self.pca.transform(x)
+                result = pd.DataFrame(result)
+                cluster_no = self.cluster.predict(result)
+                prediction = []
+                for i in range(len(cluster_no)):
+                    if(os.path.isfile(self.path + "/Model_files/Model_"+str(cluster_no[i])+"_cluster.pickle")):
+                        ML_model = pickle.load(open(self.path + "/Model_files/Models/Model_"+str(cluster_no[i])+"_cluster.pickle", "rb"))
+                        prediction.append(round(ML_model.predict(pd.DataFrame(result.iloc[i]).transpose())[0], 2))
+                    else:
+                        ML_model = pickle.load(open(self.path + "/Model_files/Models/Model_"+str(2)+"_cluster.pickle", "rb"))
+                        prediction.append(round(ML_model.predict(pd.DataFrame(result.iloc[i]).transpose())[0], 2))
+
+                self.df["SalePrice"] = prediction
+            
+                if(os.path.isdir(self.path + "/Prediction/Predicted_Data")):
+                    self.df.to_csv(self.path + "/Prediction/Predicted_Data/"+str(self.file_name) , index = False)
+                    self.runtime_logger.add_in_logs("Predicted file is generated")
+                else:
+                    os.mkdir(self.path + "/Prediction/Predicted_Data")
+                    self.df.to_csv(self.path + "/Prediction/Predicted_Data/"+str(self.file_name) , index = False)
+                    self.runtime_logger.add_in_logs("Predicted file is generated")
+
+            
+        except Exception as e:
+            self.runtime_logger.add_in_logs("Faced an error")
+            self.runtime_logger.add_in_logs("Prediction in Prediction")
+            self.runtime_logger.add_in_logs("Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.runtime_logger.add_in_logs(str(e))
+
+
+    def prediction_package(self):
+        try:
+            self.df.drop(["SalePrice"] , axis = 1 , inplace = True)
+            print(self.df.dropna(inplace = True))
+            self.name_validations()
+            self.data_validation()
+            if(self.condition):
+                self.prediction()
+        except Exception as e:
+            self.runtime_logger.add_in_logs("Faced an error")
+            self.runtime_logger.add_in_logs("Prediction in prediction package")
+            self.runtime_logger.add_in_logs("Error on line number : {}".format(sys.exc_info()[-1].tb_lineno))
+            self.runtime_logger.add_in_logs(str(e))
